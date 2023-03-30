4221e53af48d31ad026f69bf06bd20a98fc061ca
VLAD-DS-KURS <128979331+VLAD-DS-KURS@users.noreply.github.com>
2023-03-30 13:17:04 +0300

    initial commit

---
+++ game.py
@@ -0,0 +1,23 @@
+"""Игра угадай число"""
+
+import numpy as np
+
+number = np.random.randint(1, 100) # загадываем число
+
+# создание логики игры
+# считаем кол-во попыток
+count = 0
+
+while True:
+    count+=1
+    predict_number = int(input("Угадай число от 1 до 100: "))
+    
+    if predict_number > number:
+        print("Число должно быть меньше!")
+        
+    elif predict_number < number:
+        print("Число должно быть больше!")
+        
+    else:
+        print(f"Вы угадали! Это число - {number} за {count} попыток!")
+        break # конец игры и выход из циклаinitian commit 1
